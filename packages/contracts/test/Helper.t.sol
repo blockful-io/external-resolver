968b1338365aee29de6f1df55a85f1f3a2e76e7f
picolloo <lucaspicolloo@gmail.com>
2024-02-26 15:32:45 -0300

    test: public resolver read and write

---
+++ packages/contracts/test/Helper.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+abstract contract TestHelper {
+    bytes32 constant rootNode = 0x0000000000000000000000000000000000000000000000000000000000000000;
+
+    function namehash(string memory _name) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(rootNode, keccak256(abi.encodePacked(_name))));
+    }
+
+    function labelhash(string memory _name) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_name));
+    }
+}
---
+++ packages/contracts/test/PublicResolver.t.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import "./Helper.t.sol";
+import "@ens-contracts/registry/ENSRegistry.sol";
+import {PublicResolver, INameWrapper} from "@ens-contracts/resolvers/PublicResolver.sol";
+import "@ens-contracts/reverseRegistrar/ReverseRegistrar.sol";
+
+contract PublicResolverTest is Test, TestHelper {
+    PublicResolver public resolver;
+
+    function setUp() public {
+        vm.startPrank(address(1));
+        ENSRegistry registry = new ENSRegistry();
+        ReverseRegistrar registrar = new ReverseRegistrar(registry);
+
+        // .reverse
+        registry.setSubnodeOwner(rootNode, labelhash("reverse"), address(1));
+        // .addr
+        registry.setSubnodeOwner(namehash("reverse"), labelhash("addr"), address(registrar));
+
+        resolver = new PublicResolver(registry, INameWrapper(address(1)), address(1), address(registrar));
+        registrar.setDefaultResolver(address(resolver));
+
+        // .eth
+        registry.setSubnodeRecord(rootNode, labelhash("eth"), address(1), address(resolver), 100000);
+        // public.eth
+        registry.setSubnodeRecord(namehash("eth"), labelhash("public"), address(1), address(resolver), 100000);
+
+        vm.stopPrank();
+    }
+
+    function test_Text() public {
+        vm.prank(address(1));
+        resolver.setText(namehash("public.eth"), "avatar", "blockful.png");
+
+        assertEq(resolver.text(namehash("public.eth"), "avatar"), "blockful.png");
+    }
+
+    function test_Addr() public {
+        vm.prank(address(1));
+        resolver.setAddr(namehash("public.eth"), address(1));
+
+        assertEq(resolver.addr(namehash("public.eth")), address(1));
+    }
+
+    function test_AddrMultiCoin() public {
+        vm.prank(address(1));
+        resolver.setAddr(namehash("public.eth"), uint256(60), abi.encodePacked(address(1)));
+        assertEq(resolver.addr(namehash("public.eth"), uint256(60)), abi.encodePacked(address(1)));
+    }
+}
