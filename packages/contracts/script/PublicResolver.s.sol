b330e8423bd3c5f6c59c982b024ff0574c453370
Leonardo Vieira <jonaledi@gmail.com>
2024-03-06 00:10:21 -0300

    refactor: script.sol import.

--- packages/contracts/script/PublicResolver.s.sol
+++ packages/contracts/script/PublicResolver.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console} from "forge-std/Script.sol";
+//import {Script, console} from "../lib/forge-std/src/Script.sol";
 
 import "../src/Helper.sol";
 import "@ens-contracts/registry/ENSRegistry.sol";
