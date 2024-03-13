5a7ee2e4d60f9e978c619fdfc020df9c786e57a8
Leonardo Vieira <jonaledi@gmail.com>
2024-03-13 11:09:43 -0300

    feat: script to test all the project's interaction.

---
+++ packages/client/hardhat.config.ts
@@ -0,0 +1,10 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+// import "../contracts/out";
+// import "../contracts/src";
+// import "../contracts/out/OffchainResolver.sol";
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+};
+
+export default config;
