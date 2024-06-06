5845fe9d54e6ae2d70e47f2d349c4ba17ad7370e
lucas picollo <lucaspicolloo@gmail.com>
2024-06-06 09:15:37 -0300

    chore: add gateway flag to read client

--- packages/client/src/read.ts
+++ packages/client/src/read.ts
@@ -13,10 +13,15 @@ program
   .requiredOption('-r --resolver <address>', 'ENS Universal Resolver address')
   .option('-p --provider <url>', 'web3 provider URL', 'http://127.0.0.1:8545/')
   .option('-i --chainId <chainId>', 'chainId', '1337')
+  .option(
+    '-g --gateway <gateway>',
+    'gateway',
+    'http://127.0.0.1:3000/{sender}/{data}.json',
+  )
 
 program.parse(process.argv)
 
-const { resolver, provider, chainId } = program.opts()
+const { resolver, provider, chainId, gateway } = program.opts()
 
 function getChain(chainId: number) {
   for (const chain of Object.values(chains)) {
@@ -43,18 +48,22 @@ const _ = (async () => {
     name: publicAddress,
     key: 'com.twitter',
     universalResolverAddress: resolver,
+    gatewayUrls: [gateway],
   })
   const avatar = await client.getEnsAvatar({
     name: publicAddress,
     universalResolverAddress: resolver,
+    gatewayUrls: [gateway],
   })
   const address = await client.getEnsAddress({
     name: publicAddress,
     universalResolverAddress: resolver,
+    gatewayUrls: [gateway],
   })
   const name = await client.getEnsName({
     address: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
     universalResolverAddress: resolver,
+    gatewayUrls: [gateway],
   })
 
   console.log({
