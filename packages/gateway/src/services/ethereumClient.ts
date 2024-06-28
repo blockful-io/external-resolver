e2cac10a27d67baf2e928c36356c9443f552e0ff
lucas picollo <lucaspicolloo@gmail.com>
2024-06-28 16:42:48 -0300

    feat: add signatureRocover and ethClient

---
+++ packages/gateway/src/services/ethereumClient.ts
@@ -0,0 +1,54 @@
+import 'reflect-metadata'
+import {
+  Chain,
+  HttpTransport,
+  PublicClient,
+  getChainContractAddress,
+} from 'viem'
+
+import { abi } from '@blockful/contracts/out/ENSRegistry.sol/ENSRegistry.json'
+import { abi as nameWrapperABI } from '@blockful/contracts/out/NameWrapper.sol/NameWrapper.json'
+
+export class EthereumClient<chain extends Chain> {
+  private client: PublicClient<HttpTransport, chain>
+
+  private registryAddress: string
+
+  constructor(
+    client: PublicClient<HttpTransport, chain>,
+    ensRegistry?: string,
+  ) {
+    this.client = client
+    this.registryAddress =
+      ensRegistry ||
+      getChainContractAddress({
+        chain: client.chain!,
+        contract: 'ensRegistry',
+      })
+  }
+
+  async verifyOwnership(
+    node: `0x${string}`,
+    address: `0x${string}`,
+  ): Promise<boolean> {
+    if (!this.registryAddress) return false
+
+    let owner = (await this.client.readContract({
+      address: this.registryAddress as `0x${string}`,
+      abi,
+      functionName: 'owner',
+      args: [node],
+    })) as `0x${string}`
+
+    try {
+      // handling NameWrapper owner
+      owner = (await this.client.readContract({
+        address: owner,
+        abi: nameWrapperABI,
+        functionName: 'ownerOf',
+        args: [node],
+      })) as `0x${string}`
+    } catch { /** error is expected when it isn't a contract */ }
+    return owner === address
+  }
+}
---
+++ packages/gateway/src/services/signatureRecover.ts
@@ -0,0 +1,30 @@
+import { Address, recoverTypedDataAddress } from "viem";
+import { TypedSignature } from "../types";
+
+export class SignatureRecover {
+
+  async recoverMessageSigner({
+    domain,
+    signature,
+    message,
+  }: TypedSignature): Promise<Address> {
+    return await recoverTypedDataAddress({
+      signature,
+      domain,
+      message,
+      types: {
+        Message: [
+          { name: 'functionSelector', type: 'bytes4' },
+          { name: 'sender', type: 'address' },
+          { name: 'parameters', type: 'Parameter[]' },
+          { name: 'expirationTimestamp', type: 'uint256' },
+        ],
+        Parameter: [
+          { name: 'name', type: 'string' },
+          { name: 'value', type: 'string' },
+        ],
+      },
+      primaryType: 'Message',
+    })
+  }
+}
